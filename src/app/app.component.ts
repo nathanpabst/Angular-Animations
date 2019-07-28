78ab8fe1346cacf2809541a1bf66eb4817c4de5a
Nathan Pabst <Nathan.Pabst@ey.com>
2019-07-28 09:42:50 -0500

    using the void state to transition list items from the left side.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,13 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         opacity: 1,
         transform: 'translateX(0)'
       })),
-      transition('void => *', animate(300)),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ])
     ]),
   ]
 })
